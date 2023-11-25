5d03cb4357939ce576cf06ec7422578bcf4cc382
douglasmatosdev <douglasmatosdev@gmail.com>
2023-11-25 17:55:40 -0300

    Generate Fibonacci sequence

---
+++ src/javascript/easy/generate-fibonacci-sequence.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/generate-fibonacci-sequence/
+
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let current = a = b = 1;
+    yield 0; 
+
+    while (true) {
+        yield current;
+        
+        current = b;
+        b = a + b;
+        a = current;
+    }
+};
+
+
+const gen = fibGenerator();
+let result = gen.next().value; // 0
+let result2 = gen.next().value; // 1
+let result3 = gen.next().value; // 1
+let result4 = gen.next().value; // 2
+let result5 = gen.next().value; // 3
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
