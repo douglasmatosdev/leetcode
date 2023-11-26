1aa35c0cb04a2e67371a2040af5f9bd0a38dff5c
douglasmatosdev <douglasmatosdev@gmail.com>
2023-11-25 23:52:19 -0300

    to be or not to be

---
+++ src/javascript/easy/to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/to-be-or-not-to-be/
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    
+    return {
+        toBe(params) {
+            if (val !== params) throw new Error("Not Equal");
+            
+            return true;
+        },
+        notToBe(params) {
+            if (val === params) throw new Error("Equal");
+
+            return  true;
+        },
+    };
+};
+
+
+console.log(expect(5).toBe(5)); // true
+console.log(expect(5).notToBe(5)); // throws "Equal"
+console.log(expect(5).toBe(null)); // "Not Equal"
+console.log(expect(5).notToBe(null));// true
+const o = {};
+console.log(expect(o).notToBe(o));
